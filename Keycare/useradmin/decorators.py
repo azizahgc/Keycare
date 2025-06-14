749255985481537b377314c34783e866d87457ea
azizahgc <123481144+azizahgc@users.noreply.github.com>
2025-06-15 02:08:19 +0800

    Delete Keycare directory

--- Keycare/useradmin/decorators.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-
-def group_required(group_name):
-    """Decorator to check if the user belongs to a specific group."""
-    def in_group(user):
-        return user.is_authenticated and user.groups.filter(name=group_name).exists()
-    
-    return user_passes_test(in_group, login_url='login')  # Redirect to login if unauthorized
--- Keycare/usernurse/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import dashnurse_view, patientnurse_view, patientview_view
-
-urlpatterns = [
-    path('nursedashboard/', dashnurse_view, name='nursedashboard'),   
-    path('patientnurse/', patientnurse_view, name='patientnurse' ),
-    path('patientsview/<int:pk>', patientview_view, name='patientsview')
-]
